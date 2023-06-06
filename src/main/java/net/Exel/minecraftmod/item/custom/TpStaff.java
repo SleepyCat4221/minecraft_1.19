7a6ec6387c3843c836fa1c00642dad8a3e37bff6
SleepyCat4221 <erik.spiele@gmx.at>
2023-06-06 10:08:41 +0200

    added tp staff but tp only works in x direction

--- src/main/java/net/Exel/minecraftmod/item/custom/TpStaff.java
+++ src/main/java/net/Exel/minecraftmod/item/custom/TpStaff.java
@@ -1,5 +1,11 @@
 package net.Exel.minecraftmod.item.custom;
 
+import com.mojang.math.Vector3f;
+import net.minecraft.commands.arguments.coordinates.Vec2Argument;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Position;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundMoveEntityPacket;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.WorldlyContainer;
@@ -17,11 +23,13 @@ public class TpStaff extends Item  {
         super(properties);
     }
 
+    
+
 
 
 
         public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand){
-            if (!level.isClientSide() && hand == InteractionHand.MAIN_HAND) {
+            /*if (!level.isClientSide() && hand == InteractionHand.MAIN_HAND) {
                 Vec3 positionVector = new Vec3(player.getEyePosition().x() + 10, player.getEyePosition().y , player.getEyePosition().z());
                 player.setPos(positionVector.x, positionVector.y, positionVector.z);
             }
@@ -29,6 +37,34 @@ public class TpStaff extends Item  {
                 Vec3 positionVector = new Vec3(player.getEyePosition().x() + 10, player.getEyePosition().y , player.getEyePosition().z());
                 player.setPos(positionVector.x, positionVector.y, positionVector.z);
             }
+
+             */
+
+
+            if (!level.isClientSide() && hand == InteractionHand.MAIN_HAND) {
+                Vec3 pos = new Vec3(player.getX()   , player.getZ() , player.getZ());
+                Direction dir =    player.getDirection();
+
+
+                if (dir == Direction.SOUTH){
+                    player.setPos(pos.x ,pos.y + 10, pos.z);
+                }else if (dir == Direction.WEST){
+                    player.setPos(pos.x - 10,pos.y, pos.z);
+                }else if (dir == Direction.NORTH){
+                    player.setPos(pos.x ,pos.y -10, pos.z);
+                }else if (dir == Direction.EAST){
+                    player.setPos(pos.x  + 10,pos.y, pos.z);
+                }else if (dir == Direction.DOWN){
+                    player.setPos(pos.x ,pos.y , pos.z - 10);
+                }else if(dir == Direction.UP){
+                    player.setPos(pos.x ,pos.y , pos.z + 10);
+                }else {
+                    player.sendSystemMessage(Component.literal("Some Error accured!!!"));
+                }
+
+
+            }
+
 
 
             return super.use(level, player, hand);
